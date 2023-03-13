503199baf815cdf98079a6a345a082e13e7d4b2e
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-03-12 18:23:06 -0700

    still trying...

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // JS to collect payRate info
-const income = document.getElementById('income');
-const saveBtn = document.getElementById('save-btn');
+const textInput = document.getElementById('income');
+let income = 0;
+let saveBtn = document.getElementById('save-btn');
 const payType = document.getElementById('payType'); 
 let hourlyRate;
 const hoursPerWeek = 40;
 const weeksPerYear = 52;
 
 saveBtn.addEventListener('click', function() {
+    income = textInput.value;
     //if hourly rate is selected, do nothing. Income = hourly
     //if salary rate is selected, calculat hourly rate assuming 40 hour work week
     //Hourly = Salary per year / (Hours per week × Weeks per year)
@@ -21,18 +23,15 @@ saveBtn.addEventListener('click', function() {
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
 
 // Regex to locate prices
-const priceRegex = new RegExp(/(^\$)[(0-9)+.?(0-9)*]+/g);
+const priceRegex = new RegExp(/[(0-9)+.?(0-9)*]+/g);
 
-let timeCost = `(${hoursCost}h ${minutesCost})`;
-let hoursCost;
-let minutesCost;
-
+// let timeCost = `(${hoursCost}h ${minutesCost})`;
 
 // Script to run & add timeCost to page
 
-
 for(let i = 0; i < text.length; i++) {
-    if(text[i].innerHTML.includes(priceRegex)) {
-        text[i].innerHTML = text[i].innerHTML.concat(' ', timeCost);
+    
+    if(text[i].innerHTML.includes('$')) {
+        
     }
 }
