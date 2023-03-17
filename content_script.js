dac70746e1a6feb9304ce11a4af184d95e51d8eb
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-03-16 21:27:30 -0700

    Amazon works! Still working on it though...

--- content_script.js
+++ content_script.js
@@ -53,8 +53,31 @@ async function mainFuction() {
         priceHours = Math.floor(priceDisplayed / hourlyPay);
         var minuteRate = hourlyPay / 60;
         priceMinutes = Math.floor((priceDisplayed % hourlyPay) / minuteRate);
-        element.innerText = `$${priceDisplayed} (${priceHours}h ${priceMinutes}m)`;
+        element.innerText = `$${priceDisplayed.toLocaleString("en-US")} (${priceHours}h ${priceMinutes}m)`;
     });
+
+
+
+    // Quick test
+    // var pageText = document.body.innerHTML;
+
+    // document.body.innerHTML = pageText.replace(/\$([\d,]+(?:\.\d+)?)/g,
+    // function (string, c1) {
+    //     //If there are commas, get rid of them and record they were there
+    //     var comma = c1.indexOf(',') != -1;
+    //     c1 = c1.replace(/,/g, '');
+    //     //Parse and double
+    //     var value = '' + (parseFloat(c1) * 2);
+    //     //Reinsert commas if they were there before
+    //     if (comma) {
+    //         var split = value.split(".");
+    //         value = split[0].replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+    //         if(split.length > 1)
+    //             value += "."+split[1];
+    //     }
+    //     //Return with dollar sign prepended
+    //     return '$' + value;
+    // });
 
 }
 
